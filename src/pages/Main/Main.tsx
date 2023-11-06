36ad163cd4aa45bb394319643562944538a58107
Gavrikova Natalie <gavrikovanatalie@gmail.com>
2023-11-06 16:45:52 +0400

    bugfix button add style button mini

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,24 +1,36 @@
 import Button from "../../components/Button/Button";
 import Logo from "../../components/Logo/Logo";
-import s from './Main.module.css'
+import s from "./Main.module.css";
 import Union from "./components/Union/Union";
 const Main = () => {
-    const handelFunc = ()=> {
-        alert('Helo world')
-    }
-    return ( <section className={s.main__section}>
-        <div className={s.head}>
-        <Logo/>
-        <Button handelFunc={handelFunc} classNameAdd={'buttonDefault'} nameButton={'Вход'} />
+  const handelFunc = () => {
+    alert("Helo world");
+  };
+  return (
+    <section className={s.main__section}>
+      <div className={s.head}>
+        <Logo />
+        <Button
+          handelFunc={handelFunc}
+          classNameAdd={"darkBlueMini"}
+          nameButton={"Вход"}
+        />
+      </div>
+      <div className={s.text__box}>
+        <div>
+          <span className={s.main__text}>
+            Онлайн-тренировки для занятий дома
+          </span>
+          <h2 className={s.main__title}>
+            Начните заниматься спортом <br /> и улучшите качество жизни
+          </h2>
         </div>
-        <div className={s.text__box}>
-            <div >
-                <span className={s.main__text}>Онлайн-тренировки для занятий дома</span>
-                <h2 className={s.main__title}>Начните заниматься спортом <br /> и улучшите качество жизни</h2>
-            </div>
-            <div className={s.union}><Union/></div>
+        <div className={s.union}>
+          <Union />
         </div>
-    </section> );
-}
- 
+      </div>
+    </section>
+  );
+};
+
 export default Main;
